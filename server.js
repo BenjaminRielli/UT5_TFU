530d87d5f814924d5dea2951fe9886d7dd1f0050
Benjamin Rielli <101676520+BenjaminRielli@users.noreply.github.com>
2024-06-11 19:21:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// Example using Express.js
 const express = require('express');
 const app = express();
 
-// Example defining a route in Express
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 });
 
-const port = process.env.PORT || 3000; // You can use environment variables for port configuration
+const port = process.env.PORT || 3000; 
 
-// Change server.listen to app.listen
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
